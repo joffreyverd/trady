179e9914415515df7fd782e25f07f18f1f5664a3
joffreyverd <jverd@protonmail.com>
2021-05-10 23:21:07 +0200

    feat(resume): create component

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import PageTitle from '../component/pageTitle';
+import Resume from '../component/resume';
+
+const sections = [
+  { title: 'Current Balance', value: '$5000' },
+  { title: 'Fiat/Stable', value: '$3000' },
+  { title: 'Crypto', value: '$2000' },
+];
 
 const portfolio: React.FC = () => (
   <div>
     <PageTitle title='Your portfolio' />
+    <Resume sections={sections} />
   </div>
 );
 
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,8 @@ interface Menu {
     title: string,
     action: string
 }
+
+interface Balances {
+    title: string,
+    value: string
+}
