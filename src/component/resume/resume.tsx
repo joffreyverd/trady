23139a0917b6d3643723b98ad14e07fcd190a9b4
joffreyverd <jverd@protonmail.com>
2021-05-10 23:23:37 +0200

    feat(resume): create component

---
+++ src/component/resume/resume.tsx
@@ -0,0 +1,21 @@
+import styles from './resume.module.scss';
+
+interface Props {
+  sections: Balances[],
+}
+
+const Resume: React.FC<Props> = ({ sections }) => (
+  <div className={styles.resumeContainer}>
+    {
+      sections.map((section) => (
+        <div className={styles.item} key={section.title}>
+          <h2>{section.title}</h2>
+          <p className={styles.amout}>{section.value}</p>
+        </div>
+      ))
+    }
+  </div>
+
+);
+
+export default Resume;
